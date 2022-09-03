6f6cd750124629c5fb38abd8e26211e02f9d0517
Mizar Contasti <contasti.mizar@gmail.com>
2022-09-03 17:55:08 -0500

    luxury changes

--- src/components/movements/Movement.js
+++ src/components/movements/Movement.js
@@ -1,10 +1,18 @@
 import "./Movements.css";
 
 const Movement = (props) => {
+  let amount;
+
+  if (props.amount > 0) {
+    amount = `+${props.amount}`;
+  } else {
+    amount = props.amount;
+  }
+
   return (
     <div className="movement" key={Math.random()}>
-      <div className="movement__item">{props.name}</div>
-      <div className="movement__item">{props.amount}</div>
+      <div className="movement__item item-1">{props.name}</div>
+      <div className="movement__item">{amount}</div>
       <div className="movement__item">{props.ocurrence}</div>
       <div className="movement__item">{props.type}</div>
     </div>
